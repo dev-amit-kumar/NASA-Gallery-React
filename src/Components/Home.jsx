88b0a62d70d86dc35f64717da0c40f50ab9c6fd8
Amit <amitkumar.developer1@gmail.com>
2020-12-22 00:45:54 +0530

    Updated UI

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 const today_date = new Date()
 const month = today_date.getMonth()+1
-var curr = today_date.getFullYear() + '-' + month + '-' + today_date.getDate()
+var curr = today_date.getFullYear() + '-' + month + '-' + (today_date.getDate()-1)
 const nasa_url = 'https://api.nasa.gov/planetary/apod?api_key=Z1GIRvQPk9eTsR68KiZVHXf9tsnTXIPurXUeiJqV&date='
-
 class Home extends React.Component{
     constructor(){
         super()
@@ -44,13 +43,15 @@ class Home extends React.Component{
             allowFullScreen></iframe>
         }
         else{
-            media = <img src={this.state.data.url} alt="Nasa"/>
+            media = <img className="nasa-image" src={this.state.data.hdurl} alt="Nasa"/>
         }
         if(this.state.data.media_type){
             return(
                 <>
-                    <nav className="navbar navbar-dark bg-dark">
-                        <label className="navbar-brand">NASA</label>
+                    <nav className="navbar sticky-top">
+                        <a class="navbar-brand" href="#">
+                            <img className="logo" src='https://dl.dropboxusercontent.com/s/yudlkrxyr11u73t/logo.png?dl=0' alt="logo"/>
+                        </a>
                         <form className="form-inline">
                             <input className="form-control mr-sm-2" type="date" max={curr} value={this.state.date} onChange={this.changeHandler}/>
                         </form>
